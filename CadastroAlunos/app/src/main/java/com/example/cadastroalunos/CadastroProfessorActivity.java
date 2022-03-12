94d9fc9ce0149b3f9aad76b3647aad2fbe487d7e
Lucas-Martins-de-Oliveira <lcmartins.oliveira@gmail.com>
2022-03-12 12:11:36 -0300

    adicionado salvarProfessor();

--- CadastroAlunos/app/src/main/java/com/example/cadastroalunos/CadastroProfessorActivity.java
+++ CadastroAlunos/app/src/main/java/com/example/cadastroalunos/CadastroProfessorActivity.java
@@ -46,6 +46,8 @@ public class CadastroProfessorActivity extends AppCompatActivity {
             edCpfProfessor.requestFocus();
             return;
         }
+
+        salvarProfessor();
     }
 
     public void salvarProfessor() {
