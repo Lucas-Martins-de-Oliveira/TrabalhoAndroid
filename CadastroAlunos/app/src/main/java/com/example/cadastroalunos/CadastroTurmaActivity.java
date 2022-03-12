d0515a9025e6f7ef493b43aaa1f50519c0a667d1
Lucas-Martins-de-Oliveira <lcmartins.oliveira@gmail.com>
2022-03-12 16:52:20 -0300

    Implementado classes de Turma

---
+++ CadastroAlunos/app/src/main/java/com/example/cadastroalunos/CadastroTurmaActivity.java
@@ -0,0 +1,101 @@
+package com.example.cadastroalunos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.example.cadastroalunos.dao.TurmaDAO;
+import com.example.cadastroalunos.model.Turma;
+import com.example.cadastroalunos.util.Util;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class CadastroTurmaActivity extends AppCompatActivity {
+
+    private TextInputEditText edDescricaoTurma;
+    private TextInputEditText edRegimeTurma;
+    private TextInputEditText edPeriodoTurma;
+    private LinearLayout lnPrincipal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_turma);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Cadastro de Turma");
+
+        edDescricaoTurma = findViewById(R.id.edDescricaoTurma);
+        edRegimeTurma = findViewById(R.id.edRegimeTurma);
+        lnPrincipal = findViewById(R.id.lnPrincipal);
+    }
+
+    private void validaCampos() {
+        if (edDescricaoTurma.getText().toString().equals("")) {
+            edDescricaoTurma.setError("informe a Descrição da Turma!");
+            edDescricaoTurma.requestFocus();
+            return;
+        }
+
+        if (edRegimeTurma.getText().toString().equals("")) {
+            edRegimeTurma.setError("Informe o Regime da Turma!");
+            edRegimeTurma.requestFocus();
+            return;
+        }
+
+        if (edPeriodoTurma.getText().toString().equals("")) {
+            edPeriodoTurma.setError("Informe o Período da Turma!");
+            edPeriodoTurma.requestFocus();
+            return;
+        }
+
+        salvarTurma();
+    }
+
+    public void salvarTurma() {
+        Turma turma = new Turma();
+        turma.setDescricao(edDescricaoTurma.getText().toString());
+        turma.setPeriodo(edPeriodoTurma.getText().toString());
+        turma.setRegime(edRegimeTurma.getText().toString());
+
+        if (TurmaDAO.salvar(turma) > 0) {
+            setResult(RESULT_OK);
+            finish();
+        } else {
+            Util.customSnackBar(lnPrincipal, "Erro ao salvar a Turma ("+turma.getDescricao()+") " +
+                    "verifique o log", 0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu_cadastro, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mn_limpar:
+                limparCampos();
+                return true;
+            case R.id.mn_salvar:
+                validaCampos();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void limparCampos() {
+        edPeriodoTurma.setText("");
+        edRegimeTurma.setText("");
+        edDescricaoTurma.setText("");
+    }
+}
---
+++ CadastroAlunos/app/src/main/java/com/example/cadastroalunos/adapters/TurmaAdapter.java
@@ -0,0 +1,65 @@
+package com.example.cadastroalunos.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cadastroalunos.R;
+import com.example.cadastroalunos.model.Turma;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TurmaAdapter extends RecyclerView.Adapter<TurmaAdapter.TurmaViewHolder> {
+
+    private List<Turma> listaTurmas;
+    private Context context;
+
+    public TurmaAdapter(List<Turma> listaTurmas, Context context) {
+        this.listaTurmas = listaTurmas;
+        this.context = context;
+    }
+
+    public static class TurmaViewHolder extends RecyclerView.ViewHolder {
+
+        TextInputEditText edDescricaoTurma;
+        TextInputEditText edRegimeTurma;
+        TextInputEditText edPeriodoTurma;
+
+        public TurmaViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            edDescricaoTurma = (TextInputEditText)itemView.findViewById(R.id.edDescricaoTurma);
+            edPeriodoTurma = (TextInputEditText)itemView.findViewById(R.id.edPeriodoAluno);
+            edRegimeTurma = (TextInputEditText)itemView.findViewById(R.id.edRegimeTurma);
+        }
+    }
+
+    @NonNull
+    @Override
+    public TurmaAdapter.TurmaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_turma, parent, false);
+
+        TurmaAdapter.TurmaViewHolder viewHolder = new TurmaAdapter.TurmaViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TurmaAdapter.TurmaViewHolder holder, int position) {
+        Turma turma = listaTurmas.get(position);
+
+        holder.edDescricaoTurma.setText(turma.getDescricao());
+        holder.edRegimeTurma.setText(turma.getRegime());
+        holder.edPeriodoTurma.setText(turma.getPeriodo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaTurmas.size();
+    }
+}
