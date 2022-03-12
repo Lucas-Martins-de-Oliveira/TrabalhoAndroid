f1d45fcf537017277217128519e9672102f9124b
Sergio Jonas Schulz <sergioschu@gmail.com>
2022-03-12 13:22:01 -0300

    Adicionando chamada do cadastro de professor

--- CadastroAlunos/app/src/main/java/com/example/cadastroalunos/MainActivity.java
+++ CadastroAlunos/app/src/main/java/com/example/cadastroalunos/MainActivity.java
@@ -29,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListaDisciplinaActivity.class);
         startActivity(intent);
     }
+
+    public void cadastrarProfessor(View view) {
+        Intent intent = new Intent(this, ListaProfessorActivity.class);
+        startActivity(intent);
+    }
 }
